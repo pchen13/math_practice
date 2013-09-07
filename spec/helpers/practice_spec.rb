1b70dcd7ebdd9471ae282412684890a581e6196e
Pu Chen <pobuchan2010@gmail.com>
2013-09-07 11:39:42 -0400

    add practice spec

---
+++ spec/helpers/practice_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Practice do
+  describe "operator" do
+    include Practice::Operator
+    it { operators.should be_a Hash }
+    it { operator("add").should == " + " }
+    it { operator("subtract").should == " - " }
+    it { operator("multiply").should == " x " }
+    it { operator("divide").should == " ÷ " }
+  end
+  describe "calculation" do
+    include Practice::Calculation
+    context "integer" do
+      it { integer_calc("+", 1, 2).should == 3 }
+      it { integer_calc(" - ", 1, 2).should == -1 }
+      it { integer_calc(" * ", 1, 2).should == 2 }
+      it { integer_calc(".fdiv ", 1, 2).should == 0.5 }
+    end
+  end
+  
+end
